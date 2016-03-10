906ebe3e1eef0221564f2af2243dabd777385777
simgee <simgeekiz48@gmail.com>
2016-03-10 13:05:20 +0200

    decimal to binary without recursion

---
+++ dectobinary2.py
@@ -0,0 +1,13 @@
+# Python program to convert decimal number into binary number using recursive function
+global binaryform
+binaryform = ""
+n = int(input("Enter an integer: "))# Take decimal number from user
+"""Function to print binary number for the input decimal using recursion"""
+while n >= 1:
+   strdigit= str(n % 2)
+   n= n/2
+   binaryform += strdigit
+binaryform = binaryform[::-1]
+print(binaryform)
+
+
