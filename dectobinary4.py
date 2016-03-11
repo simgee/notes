13c1382b983cd25d2498490301a65d33db5791c7
simgee <simgeekiz48@gmail.com>
2016-03-11 13:50:52 +0200

    Update dectobinary4.py

--- dectobinary4.py
+++ dectobinary4.py
@@ -11,3 +11,24 @@ def binary(n, binaryform = 0,i=1):
 dec = int(input("Enter an integer: "))# Take decimal number from user
 binary(dec)
 
+
+
+
+###//////////////////////////////////////////////////////////////////
+# the other form
+# Python program to convert decimal number into binary number using recursive function
+def binary(n, binaryform,i):
+   """Function to print binary number for the input decimal using recursion"""
+   if n < 1:
+        return binaryform
+   else:
+	digit= n % 2
+	binaryform += digit*i
+	#print(binaryform)
+	i*=10
+	print(binaryform)
+	return binary(n/2, binaryform, i)
+
+dec = int(input("Enter an integer: "))# Take decimal number from user
+aa = binary(dec, 0, 1)
+print(aa)
